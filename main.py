6327733816bec11166bf301cc7c4f02710130699
lurz <lurz@umich.edu>
2021-03-29 23:20:36 -0400

    with lyrics

--- main.py
+++ main.py
@@ -6,21 +6,78 @@ import json
 import secrets as secrets
 
 app = flask.Flask(__name__)
+app.secret_key = secrets.SECRET_KEY
 client_id = secrets.CLIENT_ID
 client_secret = secrets.CLIENT_SECRET
 spotify_token_url = 'https://accounts.spotify.com/api/token'
+spotify_search_url = 'https://api.spotify.com/v1/search'
+lyrics_search_url = 'https://api.lyrics.ovh/v1/'
+
+class Track:
+    def __init__(self, data=None, lyrics=None):
+        super().__init__()
+        item = data['tracks']['items'][0]
+        self.id = item['id']
+        self.name = item['name']
+        self.popularity = item['popularity']
+        self.minute = int((int(item['duration_ms'])/(1000*60))%60)
+        self.second = int((int(item['duration_ms'])/1000)%60)
+        self.explicit = bool(item['explicit'])
+        self.album = item['album']['name']
+        self.img_src = item['album']['images'][2]['url']
+        self.lyrics = lyrics['lyrics']
+        self.next = data['tracks']['next']
+        self.prev = data['tracks']['previous']
+
+    def present(self):
+        context = {}
+        context['id'] = self.id
+        context['name'] = self.name 
+        context['popularity'] = self.popularity
+        context['minute'] = self.minute
+        context['second'] = self.second
+        context['explicit'] = self.explicit
+        context['album'] = self.album
+        context['img_src'] = self.img_src
+        context['lyrics'] = self.lyrics
+        context['next'] = self.next
+        context['prev'] = self.prev
+        return context
+
+
+class Artist:
+    def __init__(self, data=None):
+        super().__init__()
 
 @app.route('/')
-def hello_world():
+def show_index():
     client = BackendApplicationClient(client_id=client_id)
     oauth = OAuth2Session(client=client)
-    token = oauth.fetch_token(token_url=spotify_token_url, client_id=client_id,
+    flask.session['token'] = oauth.fetch_token(token_url=spotify_token_url, client_id=client_id,
         client_secret=client_secret)
 
-    params = {'q': 'justin bieber', 'type': 'artist', 'limit': 1}
-    headers = {'Authorization': token['token_type'] + " " + token['access_token']}
-    response = requests.get('https://api.spotify.com/v1/search', params=params, headers=headers)
-    data = json.loads(response.text)
+    return flask.render_template("index.html")
 
-    context = {'items': data['artists']['items'], 'images': data['artists']['items'][0]['images'][0]}
+
+@app.route('/search', methods=['GET', 'POST'])
+def post_search():
+    if 'token' not in flask.session:
+        return flask.redirect(flask.url_for('show_index'))
+
+    context = {}
+    if flask.request.method == 'POST':
+        s_artist = flask.request.form['artist']
+        s_track = flask.request.form['track']
+
+        token = flask.session['token']
+        params = {'q': f'track:{s_track} artist:{s_artist}', 'type': 'track', 'limit': 1}
+        headers = {'Authorization': token['token_type'] + " " + token['access_token']}
+        response = requests.get(spotify_search_url, params=params, headers=headers)
+        data = json.loads(response.text)
+        lyrics = json.loads(requests.get(lyrics_search_url + s_artist + '/' + s_track).text)
+
+
+        context = Track(data, lyrics).present()
+        return flask.render_template("index.html", **context)
     return flask.render_template("index.html", **context)
+
